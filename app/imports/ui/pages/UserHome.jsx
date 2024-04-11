326fc4db7adf1016e937ea1e62d94628c626c5bb
kakokapolei <95083722+kakokapolei@users.noreply.github.com>
2024-04-10 20:50:13 -1000

    Created user home page
    User home page which features six different cards and buttons for the user to choose from

--- app/imports/ui/pages/UserHome.jsx
+++ app/imports/ui/pages/UserHome.jsx
@@ -1,26 +1,84 @@
 import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
-import { PersonFill, PeopleFill, CalendarFill } from 'react-bootstrap-icons';
+import { Col, Container, Row, Card, Button } from 'react-bootstrap';
+import { PersonFill, PeopleFill, CalendarFill, GearFill, ClockHistory, CheckLg } from 'react-bootstrap-icons';
 
 /* A simple static component to render some text for the landing page. */
 const UserHome = () => (
   <Container id="user-home" fluid className="py-3">
-    <Row className="align-middle text-center">
+    <Row id="user-cards" className="align-middle text-center justify-content-center p-5">
       <Col xs={4}>
-        <PersonFill size={50} />
-        <h1>Profile</h1>
+        <Card>
+          <Card.Header>
+            <PersonFill size={100} />
+            <h4>Profile</h4>
+          </Card.Header>
+          <Card.Body>
+            <Button>Edit Profile</Button>
+          </Card.Body>
+        </Card>
       </Col>
 
       <Col xs={4}>
-        <PeopleFill size={50} />
-        <h1>Match</h1>
+        <Card>
+          <Card.Header>
+            <PeopleFill size={100} />
+            <h4>Match</h4>
+          </Card.Header>
+          <Card.Body>
+            <Button>Find other Study Buddies</Button>
+          </Card.Body>
+        </Card>
       </Col>
 
       <Col xs={4}>
-        <CalendarFill size={50} />
-        <h1>Calendar</h1>
+        <Card>
+          <Card.Header>
+            <CalendarFill size={100} />
+            <h4>Calendar</h4>
+          </Card.Header>
+          <Card.Body>
+            <Button>View Calendar</Button>
+          </Card.Body>
+        </Card>
+      </Col>
+    </Row>
+
+    <Row id="user-cards" className="align-middle text-center justify-content-center p-5">
+      <Col xs={4}>
+        <Card>
+          <Card.Header>
+            <GearFill size={100} />
+            <h4>Settings</h4>
+          </Card.Header>
+          <Card.Body>
+            <Button>Change Settings</Button>
+          </Card.Body>
+        </Card>
       </Col>
 
+      <Col xs={4}>
+        <Card>
+          <Card.Header>
+            <ClockHistory size={100} />
+            <h4>History</h4>
+          </Card.Header>
+          <Card.Body>
+            <Button>View past sessions</Button>
+          </Card.Body>
+        </Card>
+      </Col>
+
+      <Col xs={4}>
+        <Card>
+          <Card.Header>
+            <CheckLg size={100} />
+            <h4>Goals</h4>
+          </Card.Header>
+          <Card.Body>
+            <Button>View Goals</Button>
+          </Card.Body>
+        </Card>
+      </Col>
     </Row>
   </Container>
 );
