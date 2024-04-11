44490babfd7f95ef2d0620e6dc2976eaa9290cbf
Yilamu Lafeier <143864267+YilamuLafeier@users.noreply.github.com>
2024-04-10 22:28:09 -1000

    Update Calendar.jsx

--- app/imports/ui/pages/Calendar.jsx
+++ app/imports/ui/pages/Calendar.jsx
@@ -1,24 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import EventMod from './.../components/EventMod';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
+import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
+import SimpleSchema from 'simpl-schema';
+
+const EventSchema = new SimpleSchema({
+  title: String,
+  startTime: Date,
+  endTime: Date,
+  description: {
+    type: String,
+    optional: false,
+  },
+});
+
+const bridge = new SimpleSchema2Bridge(EventSchema);
 
 const Calendar = () => {
+  const [openMod, setOpenMod] = useState(false);
 
-  const handleDateClick = (arg) => {
-    alert(arg.dateStr);
+  const handleDateClick = () => {
+    setOpenMod(true);
+  };
+
+  const submit = (eventData) => {
+    const { title, startTime, endTime, description } = eventData;
+    setOpenMod(false);
   };
 
   return (
-    <FullCalendar
-      plugins={[dayGridPlugin, interactionPlugin]}
-      dateClick={handleDateClick}
-      initialView="dayGridMonth"
-      weekends
-      events={[
-        { title: 'sample1', date: '2024-04-01' },
-      ]}
-    />
+    <div>
+      <FullCalendar
+        plugins={[dayGridPlugin, interactionPlugin]}
+        dateClick={handleDateClick}
+        initialView="dayGridMonth"
+        weekends
+        events={[
+          { title: 'sample1', date: '2024-04-01' },
+        ]}
+      />
+      <EventMod isOpen={openMod} onClose={() => setOpenMod(false)} onSubmit={submit} />
+    </div>
   );
 };
 
