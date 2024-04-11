8db351ba65d312eb063a8a25f6087a1266544521
Hailey Fagaragan <156958302+haileyfagaragan@users.noreply.github.com>
2024-04-10 22:55:28 -1000

    Navbar & Footer

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -1,23 +1,47 @@
 import React from 'react';
-import { Col, Container } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
+import { Telephone, Envelope } from 'react-bootstrap-icons';
 
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 const Footer = () => (
   <footer className="mt-auto py-3 bg-light">
     <Container>
       <Col className="text-center">
-        Department of Information and Computer Sciences
+        Contact Us
         {' '}
-        <br />
-        University of Hawaii
-        <br />
-        Honolulu, HI 96822
-        {' '}
-        <br />
-        <a href="http://ics-software-engineering.github.io/meteor-application-template-react">
-          Template Home
-          Page
-        </a>
+        <hr />
+        <Row>
+          <Col>
+            <strong>Braeden Mendoza</strong>
+            <br />
+            <Envelope className="icon-margin" />
+            <Telephone className="icon-margin" />
+          </Col>
+          <Col>
+            <strong>Yilamu Lafeier</strong>
+            <br />
+            <Envelope className="icon-margin" />
+            <Telephone className="icon-margin" />
+          </Col>
+          <Col>
+            <strong>Jay Suh</strong>
+            <br />
+            <Envelope className="icon-margin" />
+            <Telephone className="icon-margin" />
+          </Col>
+          <Col>
+            <strong>Hailey Fagaragan</strong>
+            <br />
+            <Envelope className="icon-margin" />
+            <Telephone className="icon-margin" />
+          </Col>
+          <Col>
+            <strong>Kaelan Valencia</strong>
+            <br />
+            <Envelope className="icon-margin" />
+            <Telephone className="icon-margin" />
+          </Col>
+        </Row>
       </Col>
     </Container>
   </footer>
--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -16,18 +16,24 @@ const NavBar = () => {
     <Navbar bg="light" expand="lg">
       <Container>
         <Navbar.Brand as={NavLink} to="/">
-          <h2>meteor-application-template-react</h2>
+          <h2>BrainBows</h2>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto justify-content-start">
-            {currentUser ? ([
-              <Nav.Link id="add-stuff-nav" as={NavLink} to="/add" key="add">Add Stuff</Nav.Link>,
-              <Nav.Link id="list-stuff-nav" as={NavLink} to="/list" key="list">List Stuff</Nav.Link>,
-            ]) : ''}
             {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
               <Nav.Link id="list-stuff-admin-nav" as={NavLink} to="/admin" key="admin">Admin</Nav.Link>
             ) : ''}
+            {currentUser ? ([
+              // <Nav.Link id="add-stuff-nav" as={NavLink} to="/add" key="add">Add Stuff</Nav.Link>,
+              // <Nav.Link id="list-stuff-nav" as={NavLink} to="/list" key="list">List Stuff</Nav.Link>,
+              <Nav.Link id="rate-my-peer" as={NavLink} to="/ratemypeer" key="ratemypeer">Rate-My-Peer</Nav.Link>,
+            ]) : ''}
+            <Nav.Link as={NavLink} to="/register" key="profiles">Profiles</Nav.Link>
+            <Nav.Link as={NavLink} to="/calendar" key="calendar">Calendar</Nav.Link>
+            <Nav.Link as={NavLink} to="/officehours" key="officehours">Office Hours</Nav.Link>
+            <Nav.Link as={NavLink} to="/leaderboard" key="leaderboard">Leaderboard</Nav.Link>
+            <Nav.Link as={NavLink} to="/studysesh" key="studysesh">Study Sessions</Nav.Link>
           </Nav>
           <Nav className="justify-content-end">
             {currentUser === '' ? (
@@ -45,6 +51,10 @@ const NavBar = () => {
               </NavDropdown>
             ) : (
               <NavDropdown id="navbar-current-user" title={currentUser}>
+                <NavDropdown.Item id="profile" as={NavLink} to="/profile">
+                  <PersonFill />
+                  Profile
+                </NavDropdown.Item>
                 <NavDropdown.Item id="navbar-sign-out" as={NavLink} to="/signout">
                   <BoxArrowRight />
                   {' '}
