a795104f0e609da34795fc3b5a3b09e71713e57a
Hailey Fagaragan <156958302+haileyfagaragan@users.noreply.github.com>
2024-04-11 20:52:30 -1000

    updated footer

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -1,23 +1,56 @@
 import React from 'react';
-import { Col, Container } from 'react-bootstrap';
+import { CardSubtitle, Col, Container, Row } from 'react-bootstrap';
+import { Envelope, Telephone } from 'react-bootstrap-icons';
 
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 const Footer = () => (
   <footer className="mt-auto py-3 bg-light">
     <Container>
       <Col className="text-center">
-        Department of Information and Computer Sciences
+        <CardSubtitle>Brainbows</CardSubtitle>
         {' '}
         <br />
-        University of Hawaii
-        <br />
-        Honolulu, HI 96822
+        Information & Computer Sciences
         {' '}
         <br />
-        <a href="http://ics-software-engineering.github.io/meteor-application-template-react">
-          Template Home
-          Page
-        </a>
+        University of Hawaii at Manoa
+        {' '}
+        <br />
+        Project Homepage
+        {' '}
+        <hr />
+        <Row>
+          <Col>
+            <strong>Braeden Mendoza</strong>
+            <br />
+            <Envelope className="icon-margin" />
+            <Telephone className="icon-margin" />
+          </Col>
+          <Col>
+            <strong>Yilamu Lafeier</strong>
+            <br />
+            <Envelope className="icon-margin" />
+            <Telephone className="icon-margin" />
+          </Col>
+          <Col>
+            <strong>Jay Suh</strong>
+            <br />
+            <Envelope className="icon-margin" />
+            <Telephone className="icon-margin" />
+          </Col>
+          <Col>
+            <strong>Hailey Fagaragan</strong>
+            <br />
+            <Envelope className="icon-margin" />
+            <Telephone className="icon-margin" />
+          </Col>
+          <Col>
+            <strong>Kaelan Valencia</strong>
+            <br />
+            <Envelope className="icon-margin" />
+            <Telephone className="icon-margin" />
+          </Col>
+        </Row>
       </Col>
     </Container>
   </footer>
