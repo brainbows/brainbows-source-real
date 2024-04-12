fb15e9b72ae96cf3f5c91db85ab73d8dfbb45986
braeden-cs <156963237+braeden-cs@users.noreply.github.com>
2024-04-11 20:46:42 -1000

    deployment

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,22 +1,9 @@
 import { Meteor } from 'meteor/meteor';
-import { Stuffs } from '../../api/stuff/Stuff.js';
 import { Students } from '../../api/student/Student';
 
 /* eslint-disable no-console */
 
 // Initialize the database with a default data document.
-const addData = (data) => {
-  console.log(`  Adding: ${data.name} (${data.owner})`);
-  Stuffs.collection.insert(data);
-};
-
-// Initialize the StuffsCollection if empty.
-if (Stuffs.collection.find().count() === 0) {
-  if (Meteor.settings.defaultData) {
-    console.log('Creating default data.');
-    Meteor.settings.defaultData.forEach(data => addData(data));
-  }
-}
 
 const addStudent = (student) => {
   console.log(`  Adding: ${student.name} (${student.owner})`);
