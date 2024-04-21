84b449a6ef2e6a74c8a14419e0b8a157e784b228
braeden-cs <156963237+braeden-cs@users.noreply.github.com>
2024-04-20 21:43:22 -1000

    created urgent session collection

---
+++ app/imports/api/urgent/Urgent.js
@@ -0,0 +1,37 @@
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+
+/**
+ * The StudentsCollection. It encapsulates state and variable values for students.
+ */
+class UrgentCollection {
+  constructor() {
+    // The name of this collection.
+    this.name = 'UrgentCollection';
+    // Define the Mongo collection.
+    this.collection = new Mongo.Collection(this.name);
+    // Define the structure of each document in the collection.
+    this.schema = new SimpleSchema({
+      name: String,
+      owner: String,
+      class: {
+        type: String,
+        allowedValues: ['ICS 101', 'ICS 110P', 'ICS 111', 'ICS 141', 'ICS 211', 'ICS 241'],
+      },
+      topic: String,
+      startTime: String,
+      endTime: String,
+    });
+    // Attach the schema to the collection, so all attempts to insert a document are checked against schema.
+    this.collection.attachSchema(this.schema);
+    // Define names for publications and subscriptions
+    this.userPublicationName = `${this.name}.publication.user`;
+    this.adminPublicationName = `${this.name}.publication.admin`;
+  }
+}
+
+/**
+ * The singleton instance of the StuffsCollection.
+ * @type {UrgentCollection}
+ */
+export const UrgentSesh = new UrgentCollection();
--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,5 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import { Students } from '../../api/student/Student';
+import { UrgentSesh } from '../../api/urgent/Urgent';
 
 /* eslint-disable no-console */
 
@@ -14,5 +15,18 @@ if (Students.collection.find().count() === 0) {
   if (Meteor.settings.defaultStudents) {
     console.log('Creating default Students.');
     Meteor.settings.defaultStudents.forEach(student => addStudent(student));
+  }
+}
+
+const addUrgent = (urgent) => {
+  console.log(`  Adding: ${urgent.name} (${urgent.owner})`);
+  UrgentSesh.collection.insert(urgent);
+};
+
+// Initialize the StuffsCollection if empty.
+if (UrgentSesh.collection.find().count() === 0) {
+  if (Meteor.settings.defaultUrgent) {
+    console.log('Creating default Urgent Sessions.');
+    Meteor.settings.defaultUrgent.forEach(urgent => addUrgent(urgent));
   }
 }
